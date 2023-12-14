f0334b13af45bbaf7709617f94d5d92d097b2358
chrlskrt <repuestocharlene@gmail.com>
2023-12-14 09:52:54 +0800

    maze maze maze

--- src/G2_MiniGame/MAZE/MazeMenu.java
+++ src/G2_MiniGame/MAZE/MazeMenu.java
@@ -1,6 +1,9 @@
 package G2_MiniGame.MAZE;
 
-import G2_MiniGame.MAZE.Maze.MenuGamePanel;
+import G2_MiniGame.MAZE.Maze.MapFrame;
+import G2_MiniGame.MAZE.Maze.MapFrameGamePanel;
+import G2_MiniGame.Main;
+import G2_MiniGame.MiniGame_MainMenu;
 import G2_MiniGame.Player;
 import G2_MiniGame.PlayersHandler;
 
@@ -23,6 +26,11 @@ public class MazeMenu extends JFrame {
     private JLabel lblFourth;
     private JTextField tfFifth;
     private JLabel lblFifth;
+    private JButton btnHome;
+    private JButton btnPlayGame;
+    private JButton btnViewLeaderboards;
+    private JPanel jpMazeGame;
+    private JLabel lblPlayer;
     PlayersHandler handler = PlayersHandler.getInstance();
 
     public MazeMenu(){
@@ -38,15 +46,43 @@ public class MazeMenu extends JFrame {
     }
 
     private void createButtonListeners(){
+        btnHome.addActionListener(e -> {
+            playSound("src/G2_MiniGame/Audio/click.wav");
+            new MiniGame_MainMenu(true);
+        });
+
+        btnViewLeaderboards.addActionListener(e -> {
+            playSound("src/G2_MiniGame/Audio/click.wav");
+            displayLeaderboards();
+        });
+
         btnLeadHome.addActionListener(e -> {
             playSound("src/G2_MiniGame/Audio/click.wav");
             displayMenu();
         });
+
+        btnPlayGame.addActionListener(e -> {
+            playSound("src/G2_MiniGame/Audio/click.wav");
+//            displayMazeGame();
+            MapFrame mapFrame = new MapFrame();
+            mapFrame.start();
+        });
+    }
+
+    public void displayMazeGame(){
+        setContentPane(jpMazeGame);
+        revalidate();
+        repaint();
+
+        Main.stopSound();
+        new Thread((MapFrameGamePanel) jpMazeGame).start();
     }
     public void displayMenu(){
         setContentPane(jpMazeMenu);
         revalidate();
         repaint();
+
+        lblPlayer.setText(MiniGame_MainMenu.currentPlayer.getUsername());
     }
 
     public void displayLeaderboards(){
@@ -69,6 +105,6 @@ public class MazeMenu extends JFrame {
     }
 
     private void createUIComponents() {
-        jpMazeMenu = new MenuGamePanel(this);
+        jpMazeGame = new MapFrameGamePanel();
     }
 }
